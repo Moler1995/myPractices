a1532747108c0675b7b9d02d5858be838179e7c8
qianyuhang <comeonqyh@163.com>
2021-01-16 17:28:49 +0800

    Ask&Answer update 20210116

--- src/main/java/qyh/sortalgcollection/BubbleSort.java
+++ src/main/java/qyh/sortalgcollection/BubbleSort.java
@@ -1,7 +1,34 @@
 package qyh.sortalgcollection;
 
+import java.util.Arrays;
+
 /**
  * 冒泡
  */
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 从小到大
+     * @param arr arr
+     */
+    public static void sort(int[] arr) {
+        for (int i = arr.length - 1; i >=0; i--) {
+            for (int j = i; j >= 0; j--) {
+                if (arr[i] < arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
--- src/main/java/qyh/sortalgcollection/QuickSort.java
+++ src/main/java/qyh/sortalgcollection/QuickSort.java
@@ -1,7 +1,43 @@
 package qyh.sortalgcollection;
 
+import java.util.Arrays;
+
 /**
  * 快排
  */
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int target = arr[start];
+        int i = start;
+        int j = end;
+        while (i < j) {
+            if (arr[i] <= target) {
+                i++;
+            } else if (arr[j] >= target) {
+                j--;
+            } else {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+        swap(arr, start, i);
+        sort(arr, start + 1, i - 1);
+        sort(arr, i + 1, end);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
