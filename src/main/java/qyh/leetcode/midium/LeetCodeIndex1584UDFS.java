4a0cfdb242a88450245ba379b07dfc5131ad8e16
qianyuhang <comeonqyh@163.com>
2021-01-19 19:26:57 +0800

    20210119 update

---
+++ src/main/java/qyh/leetcode/midium/LeetCodeIndex1584UDFS.java
@@ -0,0 +1,14 @@
+package qyh.leetcode.midium;
+
+/**
+ * udfs
+ */
+public class LeetCodeIndex1584UDFS {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minCostConnectPoints(int[][] points) {
+        return 0;
+    }
+}
