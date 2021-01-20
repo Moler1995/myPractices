e8f522ad72d360a7914ebf6e1b9d9fe0a75a59aa
qianyuhang <comeonqyh@163.com>
2021-01-20 21:36:19 +0800

    Ask&Answer update 20210120

--- src/main/java/qyh/leetcode/hard/LeetCodeIndex10.java
+++ src/main/java/qyh/leetcode/hard/LeetCodeIndex10.java
@@ -14,7 +14,7 @@ package qyh.leetcode.hard;
  */
 public class LeetCodeIndex10 {
     public static void main(String[] args) {
-        System.out.println(isMatched("aaa", "ab*a*c*a"));
+        System.out.println(isMatched("aab", "c*a*b"));
     }
 
     public static boolean isMatch(String s, String p) {
@@ -57,7 +57,26 @@ public class LeetCodeIndex10 {
          * a 2  f f f f t t f t t
          * a 3  f f f f f t f t t
          */
-        return false;
-
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*') {
+                dp[0][i + 1] = dp[0][i - 1];
+            }
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (p.charAt(j - 2) != s.charAt(i - 1) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 }
