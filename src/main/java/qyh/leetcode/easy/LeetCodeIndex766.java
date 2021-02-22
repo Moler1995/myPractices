02961b0a7aa0bce731e44b2f18754cb21c0ae298
qianyuhang <comeonqyh@163.com>
2021-02-22 10:04:26 +0800

    20210221 update

---
+++ src/main/java/qyh/leetcode/easy/LeetCodeIndex766.java
@@ -0,0 +1,34 @@
+package qyh.leetcode.easy;
+
+/**
+ * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ *
+ * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/toeplitz-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCodeIndex766 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}};
+        System.out.println(isToeplitzMatrix(matrix));
+    }
+
+    private static boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j == 0) {
+                    continue;
+                }
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
