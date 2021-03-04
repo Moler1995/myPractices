65d5b7c8142d15748917559b421375425edcd60e
qianyuhang <comeonqyh@163.com>
2021-03-04 17:08:01 +0800

    update 20210304

---
+++ src/main/java/qyh/leetcode/midium/LeetCodeIndex354.java
@@ -0,0 +1,41 @@
+package qyh.leetcode.midium;
+
+import java.util.*;
+
+/**
+ * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式(w, h)出现。当另一个信封的宽度和高度都比这个信封大的时候，
+ * 这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ *
+ * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/russian-doll-envelopes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCodeIndex354 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
+            } else {
+                return b[1] - a[1];
+            }
+        });
+        int[] dp = new int[envelopes.length];
+        int res = 1;
+        for (int i = 0; i < envelopes.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
